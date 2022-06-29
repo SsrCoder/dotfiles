e5836f1979126ab56e081ca738c28cf46db30c39
bianbinfeng <bianbinfeng@zhihu.com>
2022-06-30 02:47:11 +0800

    add vim.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 
-curl -fsSL https://github.com/TheLocehiliosan/yadm/raw/master/yadm | bash -s clone https://github.com/SsrCoder/dotfiles.git -b bootstrap_vim --bootstrap
+curl -fsSL https://github.com/TheLocehiliosan/yadm/raw/master/yadm | bash -s clone https://github.com/SsrCoder/dotfiles.git -b main --bootstrap
