96e4205bdaac3661e17dc8537051273302f5642c
SsrCoder <SsrCoder@gmail.com>
2022-06-30 02:51:19 +0800

    feat: add vim bootstrapping (#1)
    feat: add vim bootstrapping
    
    Co-authored-by: bianbinfeng <bianbinfeng@zhihu.com>

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ mkdir $HOME/bin
 wget -O $HOME/bin/yadm https://raw.githubusercontent.com/TheLocehiliosan/yadm/master/yadm
 chmod +x $HOME/bin/yadm
 export PATH=$PATH:$HOME/bin
-yadm clone https://github.com/SsrCoder/dotfiles.git -b main
-cd $HOME && yadm submodule update --init --recursive
+yadm clone https://github.com/SsrCoder/dotfiles.git -b main --no-bootstrap
+cd $HOME && yadm submodule update --init --recursive && yadm bootstrap
