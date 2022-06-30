f1abe0721c9bdf0732a8375d91ca4da90787f89c
bianbinfeng <bianbinfeng@zhihu.com>
2022-06-30 16:52:07 +0800

    fix: shell script err

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ mkdir $HOME/bin
 wget -O $HOME/bin/yadm https://raw.githubusercontent.com/TheLocehiliosan/yadm/master/yadm
 chmod +x $HOME/bin/yadm
 export PATH=$PATH:$HOME/bin
-yadm clone https://github.com/SsrCoder/dotfiles.git -b main --no-bootstrap
+yadm clone https://github.com/SsrCoder/dotfiles.git --no-bootstrap
+yadm checkout -f main
 cd $HOME && yadm submodule update --init --recursive && yadm bootstrap
