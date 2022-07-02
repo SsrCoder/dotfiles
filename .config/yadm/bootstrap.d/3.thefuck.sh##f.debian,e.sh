be1a680f3de6a8997de3290c64e85cbc169a7166
SsrCoder <SsrCoder@gmail.com>
2022-07-02 04:18:02 +0000

    thefuck

--- .config/yadm/bootstrap.d/3.thefuck.sh##f.debian,e.sh
+++ .config/yadm/bootstrap.d/3.thefuck.sh##f.debian,e.sh
@@ -4,10 +4,10 @@ if ! command -v fuck >/dev/null 2>&1; then
     if [[ "$(whoami)" == "root" ]]; then
         echo "Installing fuck"
         apt-get update
-        apt-get install -y fuck
+        apt-get install -y thefuck
     else
         echo "Installing fuck"
         sudo apt-get update
-        sudo apt-get install -y fuck
+        sudo apt-get install -y thefuck
     fi
 fi
