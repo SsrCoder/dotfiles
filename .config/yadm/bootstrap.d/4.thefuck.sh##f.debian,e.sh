571d14d9a7f7f36f8ea1d74fdaf9e450e6815103
SsrCoder <SsrCoder@gmail.com>
2022-07-02 04:03:10 +0000

    zsh

---
+++ .config/yadm/bootstrap.d/4.thefuck.sh##f.debian,e.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v fuck >/dev/null 2>&1; then
+    if [[ "$(whoami)" == "root" ]]; then
+        echo "Installing fuck"
+        apt-get update
+        apt-get install -y fuck
+    else
+        echo "Installing fuck"
+        sudo apt-get update
+        sudo apt-get install -y fuck
+    fi
+fi
